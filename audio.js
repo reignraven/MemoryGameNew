8d3bfe07738a5abdbb3912ace0654653e040d55b
reignraven <maryjoanneolino@gmail.com>
2017-03-04 15:54:50 +0800

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,17 @@
+<!-- START SOUND CODE V4.1a HTML5 -->
+<script type="text/javascript">
+
+// PLAYER VARIABLES
+
+var mp3snd = "Pou - Sky Jump  Music.mp3";
+
+document.write('<audio autoplay="autoplay">');
+document.write('<source src="'+mp3snd+'" type="audio/mpeg">');
+document.write('<!--[if lt IE 9]>');
+document.write('<bgsound src="'+mp3snd+'" loop="1">');
+document.write('<![endif]-->');
+document.write('</audio>');
+
+</script>
+<br>
+<!-- END SOUND CODE V4.1a -->
---
+++ pics.js
@@ -0,0 +1,51 @@
+//<script type="text/javascript">
+var memory_array = new Array();
+
+memory_array[0] = new Image();
+memory_array[0].src = 'img/8.jpg';
+
+memory_array[1] = new Image();
+memory_array[1].src = 'img/9.jpg';
+
+memory_array[2] = new Image();
+memory_array[2].src = 'img/10.jpg';
+
+memory_array[3] = new Image();
+memory_array[3].src = 'img/11.jpg';
+
+memory_array[4] = new Image();
+memory_array[4].src = 'img/1.jpg';
+
+memory_array[5] = new Image();
+memory_array[5].src = 'img/2.jpg';
+
+memory_array[6] = new Image();
+memory_array[6].src = 'img/3.jpg';
+
+memory_array[7] = new Image();
+memory_array[7].src = 'img/4.jpg';
+
+memory_array[8] = new Image();
+memory_array[8].src = 'img/5.jpg';
+
+memory_array[9] = new Image();
+memory_array[9].src = 'img/6.jpg';
+
+memory_array[10] = new Image();
+memory_array[10].src = 'img/7.jpg';
+
+memory_array[11] = new Image();
+memory_array[11].src = 'img/8.jpg';
+
+memory_array[8] = new Image();
+memory_array[8].src = 'img/12.jpg';
+
+memory_array[9] = new Image();
+memory_array[9].src = 'img/13.jpg';
+
+memory_array[10] = new Image();
+memory_array[10].src = 'img/14.jpg';
+
+memory_array[11] = new Image();
+memory_array[11].src = 'img/15.jpg';
+//</script>
